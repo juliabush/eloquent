572b144d05645e5fddd51248b61fab19fccf47ca
Julia Bush <j.elizabethbush@gmail.com>
2025-09-01 13:19:36 +0200

    Creating a function to add books to my library array

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-}
-const player = new Player("steve", "X");
-console.log(player.name);
+const myLibrary = [];
+
 
 function Books(title, author, pages, readstatus) {
     this.title = title;
@@ -18,3 +14,12 @@ function Books(title, author, pages, readstatus) {
 const theHobbit = new Books("The Hobbit", "J.R.R Tolkien", "295", "Not Read Yet");
 
 console.log(theHobbit.info())
+
+function addBookToLibrary (title, author, pages, readstatus) {
+    let createBook = new Books(title, author, pages, readstatus);
+    myLibrary.push(createBook);
+    return myLibrary;
+}
+console.log(addBookToLibrary("Oliver Twist", "Charles Dickens", "342", "Read"));
+
+console.log(myLibrary)
